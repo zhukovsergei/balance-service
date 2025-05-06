618ddbbd2814a5e1c8c97e14ac88755e04b98eed
Zhukov Sergei <alivesystemsonly@gmail.com>
2025-05-06 20:12:24 +0200

    Added uuid for events

--- src/main/java/ch/fdsgn/balance_service/event/FundsDepositedEvent.java
+++ src/main/java/ch/fdsgn/balance_service/event/FundsDepositedEvent.java
@@ -2,13 +2,22 @@ package ch.fdsgn.balance_service.event;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.UUID;
 
 public record FundsDepositedEvent(
+        UUID eventId,
         String accountId,
         BigDecimal amount,
         Instant timestamp
 ) {
     public FundsDepositedEvent(String accountId, BigDecimal amount) {
-        this(accountId, amount, Instant.now());
+        this(UUID.randomUUID(), accountId, amount, Instant.now());
+    }
+
+    public FundsDepositedEvent(UUID eventId, String accountId, BigDecimal amount, Instant timestamp) {
+        this.eventId = eventId;
+        this.accountId = accountId;
+        this.amount = amount;
+        this.timestamp = timestamp;
     }
 } 
